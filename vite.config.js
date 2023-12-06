2f9045ec480306be702ed134d5465dfb6af6dc85
phantien295 <phantien295@gmail.com>
2023-12-06 15:33:14 +0700

    [Update] base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/phantien295/",
+  // base: "/phantien295/",
   build: {
     chunkSizeWarningLimit: 1000,
   },
